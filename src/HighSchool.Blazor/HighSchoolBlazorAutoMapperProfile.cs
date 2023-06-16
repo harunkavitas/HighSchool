2bb9cdc54abd911a1fd91495bd7261d1d19be4ef
harunkavitas <harun.kasus@outlook.com>
2023-06-16 20:40:57 +0300

    Part 3
    -Maked Crud operations in Razor page
    -Created "MapperProfile" class
    -Running succesfull

---
+++ src/HighSchool.Blazor/HighSchoolBlazorAutoMapperProfile.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using HighSchool.Courses;
+
+namespace HighSchool.Blazor
+{
+    public class HighSchoolBlazorAutoMapperProfile : Profile
+    {
+        public HighSchoolBlazorAutoMapperProfile()
+        {
+            CreateMap<CourseDto, CreateUpdateCourseDto>();
+        }
+    }
+}
