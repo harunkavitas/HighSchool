248c9bdc4d17fba52a8243063bd1b7a1afd5305d
harunkavitas <harun.kasus@outlook.com>
2023-06-16 23:36:54 +0300

    Part 4
    -Created test class in Application.Tests for testing project
    - 3 test method codes written correct, valid  and fail.
    -Finally test running and result is successful.

---
+++ test/HighSchool.Application.Tests/Courses/CourseAppService_Tests.cs
@@ -0,0 +1,73 @@
+﻿using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Validation;
+using Xunit;
+
+namespace HighSchool.Courses
+{
+    public class CourseAppService_Tests :HighSchoolApplicationTestBase
+    {
+        private readonly ICourseAppService _courseAppService;
+
+        public CourseAppService_Tests()
+        {
+            _courseAppService = GetRequiredService<ICourseAppService>();
+        }
+
+        [Fact]
+        public async Task Should_Get_List_Of_Course()
+        {
+            //Act
+            var result = await _courseAppService.GetListAsync(
+                new PagedAndSortedResultRequestDto()
+            );
+
+            //Assert
+            result.TotalCount.ShouldBeGreaterThan(0);
+            result.Items.ShouldContain(b => b.Name == "Algorithm");
+        }
+        [Fact]
+        public async Task Should_Create_A_Valid_Course()
+        {
+            //Act
+            var result = await _courseAppService.CreateAsync(
+                new CreateUpdateCourseDto
+                {
+                    Name = "New test Course #1",
+                    Price = 10,
+                    PublishDate = DateTime.Now,
+                    Type = CourseType.Literature
+                }
+            );
+
+            //Assert
+            result.Id.ShouldNotBe(Guid.Empty);
+            result.Name.ShouldBe("New test Course #1");
+        }
+        [Fact]
+        public async Task Should_Not_Create_A_Course_Without_Name()
+        {
+            var exception = await Assert.ThrowsAsync<AbpValidationException>(async () =>
+            {
+                await _courseAppService.CreateAsync(
+                    new CreateUpdateCourseDto
+                    {
+                        Name = "",
+                        Price = 10,
+                        PublishDate = DateTime.Now,
+                        Type = CourseType.Deutsch
+                    }
+                );
+            });
+
+            exception.ValidationErrors
+                .ShouldContain(err => err.MemberNames.Any(mem => mem == "Name"));
+        }
+
+    }
+}
